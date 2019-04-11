095aaebab168297235cbb6c83e08a10d912891b8
--global <juanpam68@gmail.com>
2019-04-10 18:08:06 -0700

    added empty desc to models

--- cryptocurrency/models/res_currency.py
+++ cryptocurrency/models/res_currency.py
@@ -5,6 +5,7 @@ from odoo.addons import decimal_precision as dp
 
 class ResCurrency(models.Model):
     _inherit = "res.currency"
+    _description = ''
 
     rate = fields.Float(digits=dp.get_precision('Currency Precision'))
     rounding = fields.Float(digits=dp.get_precision('Currency Precision'))
@@ -12,5 +13,6 @@ class ResCurrency(models.Model):
 
 class ResCurrencyRate(models.Model):
     _inherit = "res.currency.rate"
+    _description = ''
 
     rate = fields.Float(digits=dp.get_precision('Currency Precision'))
