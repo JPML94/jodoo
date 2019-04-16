fb4e7b253689f83c3efe149de2676f71923337ea
JPML94 <juanpam68@gmail.com>
2019-04-15 22:45:14 -0700

    fix

--- school/__manifest__.py
+++ school/__manifest__.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 {
     'name': "school",
-
     'summary': """
       Quick module for student records for practice""",
-
     'description': """
-    Record Student Information 
+    Record Student Information
     """,
-
     'author': "JPML",
     'website': "https://jpml.online",
     'category': 'Tools',
     'version': '0.1',
     'depends': ['base'],
-    'data': [
-      'security/ir.model.access.csv',
-      'views/student_view.xml'
-    ],
+    'data': ['security/ir.model.access.csv', 'views/student_view.xml'],
     'installable': True,
     'application': False,
     'auto_install': False,
--- school/models/student.py
+++ school/models/student.py
@@ -2,17 +2,22 @@
 
 from odoo import models, fields
 
+
 class StudentStudent(models.Model):
     _name = 'student.student'
+    _description = 'A student record in the school module'
 
     name = fields.Char(string='Name', required=True)
     age = fields.Integer(string='Age')
     photo = fields.Binary(string='Image')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')],
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'),
+                               ('others', 'Others')],
                               string='Gender')
     student_dob = fields.Date(string='Date of Birth')
-    student_blood_group = fields.Selection(
-        [('A+', 'A+ve'), ('B+', 'B+ve'), ('O+', 'O+ve'), ('AB+', 'AB+ve'),
-         ('A-' 'A-ve'), ('B-', 'B-ve'), ('O-', 'O-ve'), ('AB-', 'AB-ve')],
-        string='Blood Group')
+    student_blood_group = fields.Selection([('A+', 'A+ve'), ('B+', 'B+ve'),
+                                            ('O+', 'O+ve'), ('AB+', 'AB+ve'),
+                                            ('A-'
+                                             'A-ve'), ('B-', 'B-ve'),
+                                            ('O-', 'O-ve'), ('AB-', 'AB-ve')],
+                                           string='Blood Group')
     nationality = fields.Many2one('res.country', string='Nationality')
