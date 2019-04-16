8a3159e260f2179c6733e91bc77144627ddbf5ca
JPML94 <juanpam68@gmail.com>
2019-04-15 19:08:59 -0700

    created school module

---
+++ school/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "school",
+
+    'summary': """
+      Quick module for student records for practice""",
+
+    'description': """
+    Record Student Information 
+    """,
+
+    'author': "JPML",
+    'website': "https://jpml.online",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+      'security/ir.model.access.csv',
+      'views/student_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
