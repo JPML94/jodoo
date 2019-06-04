b2f519f09c649563bcfeaebc0b2bd51e6c699e43
JPML94 <juanpam68@gmail.com>
2019-06-04 10:55:26 -0700

    Initial open academy init

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "jpm",
+    'website': "www.odoo.com",
+    'category': 'Demo',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
