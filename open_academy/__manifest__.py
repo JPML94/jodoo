44eae78779a1a9951576885105c0a63a4a6efbc6
JPML94 <juanpam68@gmail.com>
2019-06-04 14:19:27 -0700

    [FIX] added triagram on module name

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Open Academy",
+    'name': "Open Academy - JPM",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
