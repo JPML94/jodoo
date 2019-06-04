e4d7df08302cd43be1eb0d00246955c2c4c8be9d
JPML94 <juanpam68@gmail.com>
2019-06-04 15:27:10 -0700

    [ADD] added _rec_name and _description

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -4,6 +4,8 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'open_academy.course'
+    _description = 'This is the course model of the module Open Academy.'
+    _rec_name = 'title'
 
     title = fields.Char()
     level = fields.Selection([
