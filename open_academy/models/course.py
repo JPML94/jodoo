0295f7ea5149bc08fc7cc39891570c81b27e844e
JPML94 <juanpam68@gmail.com>
2019-06-04 15:59:11 -0700

    [ADD] added session model & modified course

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -5,12 +5,10 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'open_academy.course'
     _description = 'This is the course model of the module Open Academy.'
-    _rec_name = 'title'
 
-    title = fields.Char()
+    name = fields.Char(string='Title', required=True)
     level = fields.Selection([
         ('easy', 'Easy'),
         ('normal', 'Normal'),
         ('hard', 'Hard'),
-    ])
-    
+    ], string='Difficulty')
---
+++ open_academy/models/session.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+class Session(models.Model):
+    _name = 'open_academy.session'
+    _description = 'This is the session model for the Open Academy module.'
+
+    name = fields.Char(string='Title', required=True)
+    date_start = fields.Date(string='Start Date')
+    date_stop = fields.Date(string='Stop Date')
+    course = fields.Many2one('open_academy.course', ondelete='')
