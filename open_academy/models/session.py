10287c1804ff325702e75acd34328090a6b4a895
JPML94 <juanpam68@gmail.com>
2019-06-06 10:05:13 -0700

    [ADD] added course and teacher form views

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -6,7 +6,7 @@ class Session(models.Model):
     _name = 'open_academy.session'
     _description = 'This is the session model for the Open Academy module.'
 
-    name = fields.Char(string='Title', required=True)
+    name = fields.Char(string='Session Title', required=True)
     date_start = fields.Date(string='Start Date')
     date_stop = fields.Date(string='Stop Date')
     course_id = fields.Many2one('open_academy.course', required=True)
